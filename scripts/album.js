b509f93c7deeaab12e5904f51d6bd7ac73093a80
Thomas Caruthers <thomas.caruthers@gmail.com>
2016-06-14 18:54:39 -0700

    Assignment 25 complete

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+// sample album
+var albumHighlighter = {
+     title: 'Accent Orange',
+     artist: 'Sharpie',
+     label: 'Funk',
+     year: '1991',
+     albumArtUrl: 'assets/images/album_covers/15.png',
+     songs: [
+         { title: 'Agent Orange', duration: '3:01' },
+         { title: 'All the colors', duration: '4:15' },
+         { title: 'Pocket', duration: '2:42'},
+         { title: 'All out?', duration: '3:21' },
+         { title: 'Caps off!', duration: '6:05'}
+     ]
+ };
 
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
@@ -41,25 +56,25 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+// Select elements that we went to populate with text dynamically
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 // set the current album
 var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
+     // Assign values to each part of the album (text, images)
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
+     // Clear contents of album song list container
      albumSongList.innerHTML = '';
  
-     // #4
+     // Build list of songs from album JavaScript object
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -67,4 +82,19 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var albums = [albumPicasso, albumMarconi, albumHighlighter];
+     
+     var index = 1;
+     
+     albumImage.addEventListener("click", function(event){
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length){
+             index =0;
+         }
+     });
  };
+
+
+
